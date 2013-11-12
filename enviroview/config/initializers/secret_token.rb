9f016a2c791de890584a0cb14dda2dcfeb8c0801
Dan Crews <dancrews@me.com>
2013-11-12 09:46:19 -0500

    initial Rails framework commit.

---
+++ enviroview/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Enviroview::Application.config.secret_key_base = '0d32e8a84e1f1d3d3585dfe23430581ce21fc8c05ac573582538a5830803c17959406e48f14fa45317e613ebde4582569ba551ccc3cf8a279a7050ca83c898fb'
